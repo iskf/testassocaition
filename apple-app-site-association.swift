940e6d47e1888bfd89ae266ff384edc8b7dce383
kaifeng <24272779@qq.com>
2019-11-25 16:53:39 +0800

    1
    1

---
+++ apple-app-site-association.swift
@@ -0,0 +1,14 @@
+{
+  "applinks": {
+    "apps": [],
+    "details": [
+    {
+      "appID": "HY82F2CRLF.com.innoways.ibeacon-staff",
+      "paths": ["*"]
+    }, {
+      "appID": "HY82F2CRLF.com.innoways.ibeacon-staff",
+      "paths": ["*"]
+    }
+    ]
+  }
+}
